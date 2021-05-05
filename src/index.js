46ea0b2edf3fb52d539c26d6626945cc7afa5b09
Fernando Cardoso <fernandocardoso@live.com>
2021-05-05 14:17:14 -0300

    challenge 021 completed

--- src/index.js
+++ src/index.js
@@ -10,19 +10,73 @@ app.use(cors());
 const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers;
+
+  const user = users.find((user) => user.username === username);
+
+  if(!user){
+    return response.status(404).json({error: 'User not exists'});
+  }
+
+  request.user = user;
+
+  return next();
 }
 
 function checksCreateTodosUserAvailability(request, response, next) {
-  // Complete aqui
+  const { user } = request;
+
+   if(user.pro === false && user.todos.length < 10 || user.pro === true){
+    return next();
+  } 
+  
+  return response.status(403).json({ error: 'The free plan limit has been reached. Upgrade to the Pro plan.'})
+
 }
 
 function checksTodoExists(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers;
+  const { id } = request.params;
+
+  // validação usuário
+  const user = users.find((user) => user.username === username);
+
+  if(!user){
+    return response.status(404).json({error: 'User not exists'});
+  }
+
+  // validação id
+  const validId = validate(id);
+  if(!validId) {
+    return response.status(400).json({error: 'The available id is not a uuid.'})
+  }
+
+  // validação todo do usuário
+  const todo = user.todos.find(todo => todo.id === id);
+  if (!todo) {
+    return response.status(404).json({ error: 'The todo does not belong to the user' });
+  }
+
+  request.user = user;
+  request.todo = todo;
+
+  return next();
+
 }
 
 function findUserById(request, response, next) {
-  // Complete aqui
+  const { id } = request.params;
+
+  const user = users.find((user) => user.id === id);
+
+  if(!user){
+    return response.status(404).json({error: 'User not exists'});
+  }
+
+  request.user = user;
+
+  return next();
+
 }
 
 app.post('/users', (request, response) => {
